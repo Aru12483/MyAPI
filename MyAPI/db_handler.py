a56cd8b37b823b36c725ea2713d003eaadd82cae
Aru12483 <100664068+Aru12483@users.noreply.github.com>
2022-08-18 12:21:54 +0530

    Delete db_handler.py

--- MyAPI/db_handler.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-DATABASE_URL = 'mysql+pymysql://root:Abcd1234!@localhost:3306/inference_db'
-
-db_engine =  create_engine(DATABASE_URL)
-Session_Local =  sessionmaker(autocommit=False, autoflush=False, bind=db_engine)
-
-Base = declarative_base()
